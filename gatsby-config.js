b930cbef7eec417e1c47dcecfe52275f34252cef
Aivan Monceller <aivan@monceller.com>
2019-10-01 22:58:30 +0800

    feat(favicon): add to manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`
+        icon: `src/images/pca.png`
       }
     },
     {
