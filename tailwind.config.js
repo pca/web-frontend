9bbabe28875503175fd0534d471c6e4750f268ac
yelaD <dale@interold.com>
2020-11-10 21:17:57 +0800

    added new site

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#FFE16C",
+        darkGray: "#686465",
+        lightGray: "#e9e5e6",
+        orange: "#FF8500",
+      },
+      fontFamily: {
+        poppins: "Poppins, sans-serif",
+      },
+      borderWidth: {
+        bar: "40px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
