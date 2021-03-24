838a6f68f2ee15f0dd063a001a22c4d733b0e3c9
Simon <lim.simon10@gmail.com>
2021-03-24 16:01:33 +0800

    Making the rankings table responsive (side-scrollable) on mobile, and show a notice if results are blank

--- src/components/rankingComponents/RankingList.js
+++ src/components/rankingComponents/RankingList.js
@@ -10,38 +10,41 @@ const RankingList = props => {
 
   if (!props.isLoading) {
     content = (
-      <div className="px-3">
 
+      <div className="rankings-container px-3 md:px-0">
+        <div className="table-responsive">
 
-        <table className="rankings font-rubik text-sm">
+          <table className="rankings-list font-rubik text-sm">
 
-          <thead className="font-bold text-left">
-            <tr>
-              <th className="pos px-3 py-1">#</th>
-              <th className="name px-3 py-1">Name</th>
-              <th className="result px-3 py-1">Result</th>
-              <th className="competition px-3 py-1">Competition</th>
-              <th></th>
-            </tr>
-          </thead>
+            <thead className="font-bold text-left">
+              <tr>
+                <th className="pos px-3 py-1">#</th>
+                <th className="name px-3 py-1">Name</th>
+                <th className="result px-3 py-1">Result</th>
+                <th className="competition px-3 py-1">Competition</th>
+                <th></th>
+              </tr>
+            </thead>
 
-          <tbody>
-            {props.rankings.map((cuber, i) => {
-              return (
-                <tr key={cuber.wca_id}>
-                  <td className="pos px-3 py-1">{i + 1}</td>
-                  <td className="name px-3 py-1">{cuber.person_name}</td>
-                  <td className="result px-3 py-1"> {cuber.value}</td>
-                  <td className="competition px-3 py-1">{cuber.competition.name}</td>
-                  <td></td>
-                </tr>
-              )
-            })} 
-          </tbody>
+            <tbody>
 
-        </table>
+              {props.rankings.map((cuber, i) => {
+                return (
+                  <tr key={cuber.wca_id}>
+                    <td className="pos px-3 py-1 text-gray-600">{i + 1}</td>
+                    <td className="name px-3 py-1 text-gray-600">{cuber.person_name}</td>
+                    <td className="result px-3 py-1 font-black text-gray-600"> {cuber.value}</td>
+                    <td className="competition px-3 py-1 text-gray-600">{cuber.competition.name}</td>
+                    <td></td>
+                  </tr>
+                )
+              })}
+              {(!props.rankings?.length) ? <tr><td></td><td className="pos px-3 py-1">No results yet for this category.</td><td></td><td></td></tr> : ''}
+            </tbody>
 
+          </table>
 
+        </div>
       </div>
     )
   }
