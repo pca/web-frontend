37ee0aef0d0c0450be0c5fe45e2c1a5ba02a95c0
Simon <lim.simon10@gmail.com>
2021-04-12 19:00:12 +0800

    Creating this branch to save the code for pages that are still ongoing with development

--- src/components/rankingComponents/RegionSelect.js
+++ src/components/rankingComponents/RegionSelect.js
@@ -42,7 +42,7 @@ const RegionSelect = props => {
               return (
                 <option
                   key={region.id}
-                  value={region.id} 
+                  value={region.id}
                 >
                   {region.name}
                 </option>
